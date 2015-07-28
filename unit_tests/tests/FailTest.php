317a0e28530a82087216e3f19633fac274132605
Alex Lang <alex.lang@gmail.com>
2015-07-28 14:56:44 -0700

    test true

--- unit_tests/tests/FailTest.php
+++ unit_tests/tests/FailTest.php
@@ -5,7 +5,7 @@ class FailTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1,1);
     }
 
-    public function testFalse() {
+    public function _testFalse() {
         $this->assertEquals(2,1);
     }
 }
