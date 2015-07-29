500608f7512970f4a3e200962fed88c32a88f41a
Alexander Lang <alex.lang@gmail.com>
2015-07-29 01:20:57 -0700

    try mysql

---
+++ unit_tests/mysql_dump.sql
@@ -0,0 +1,7 @@
+create database test;
+use test;
+create table test (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
--- unit_tests/tests/Test.php
+++ unit_tests/tests/Test.php
@@ -8,5 +8,16 @@ class Test extends PHPUnit_Framework_TestCase
     public function _testFalse() {
         $this->assertEquals(2,1);
     }
+
+    public function testDB() {
+        $con = mysql_connect('127.0.0.1', 'root', '');
+        mysql_select_db('test');
+        mysql_query("insert into test (name) values ('asd')");
+        $res = mysql_query("select count(*) from test");
+        while ($row = mysql_fetch_array($res)) {
+            $this->assertEquals(1,$row[0]);
+        } 
+        mysql_close($con); 
+    }
 }
 ?>
